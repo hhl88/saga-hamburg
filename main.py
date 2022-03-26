c2d19fff82bb5ac3875f0b3ee2b071ce35aee27e
le <le@silpion.de>
2022-03-26 13:22:11 +0100

    ...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 from comparator import SupportedComparator
 from app import App
 
@@ -5,6 +6,9 @@ infos = [
     SupportedComparator(min_rooms=4, max_total_rent=1300),
     SupportedComparator(min_rooms=3, max_total_rent=1300, is_house=True),
 ]
+
 if __name__ == "__main__":
     app = App(comparators=infos)
-    app.run()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(app.run, "saga-hamburg-cronjob", day_of_week="mon-sat", hour='8-18', minute="*")
+    scheduler.start()
